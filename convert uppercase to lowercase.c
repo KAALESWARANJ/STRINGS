36aa2b38a5ea90e875ee2eb0b7bd10f57727fb97
SATHYADEVIK <34641557+SATHYADEVIK@users.noreply.github.com>
2018-05-26 21:07:02 +0530

    Create convert uppercase to lowercase.c

---
+++ convert uppercase to lowercase.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<string.h>
+int main(void)
+{
+	char s[100];
+	int i;
+	gets(s);
+	for(i=0;s[i]!='\0';i++)
+	{
+		if(s[i]>=65 && s[i]<=90)
+		{
+		printf("%c",s[i]+32);
+		}
+		else
+		{
+		printf("%c",s[i]);
+		}
+	
+	}
+return 0;
+}
+	
